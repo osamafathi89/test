a9ed15942334b7b0a484e66d9626ee7e1ee41108
osamafathi89 <osama.fathi1@gmail.com>
2018-08-07 13:42:31 +0200

    first

---
+++ whatever.js
@@ -0,0 +1,12 @@
+function makeid()
+{
+var l = num.value;	
+var text = "";
+var char_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+for(var i=0; i < l; i++ )
+{  
+text += char_list.charAt(Math.floor(Math.random() * char_list.length));
+}
+alert(text);
+return text;
+}
